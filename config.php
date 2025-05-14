8a1627c9fa3c396606e588f244586971628df200
Ravnor <177868910+Ravnor@users.noreply.github.com>
2025-05-14 16:36:23 +0200

    Create config.php
    Config.php Review

---
+++ config.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+$host = 'localhost';
+$db   = 'todolist';
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;
+dbname=$db;
+charset=$charset";
+
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+];
+
+try {
+     $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+     die("Conexion Error: " . $e->getMessage());
+}
+  
+
